412df0a73837a96ea7501cab29cabd106190bb88
Debasish Sahoo <debasishsahoo8523@gmailc.om>
2021-07-10 12:40:10 +0530

    dev8523 Add whole dir

---
+++ src/main/java/com/debasish/findexecutiontimeofanAPI/FindExecutionTimeOfAnApiApplication.java
@@ -0,0 +1,13 @@
+package com.debasish.findexecutiontimeofanAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindExecutionTimeOfAnApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindExecutionTimeOfAnApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/debasish/findexecutiontimeofanAPI/FindExecutionTimeOfAnApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.debasish.findexecutiontimeofanAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindExecutionTimeOfAnApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
